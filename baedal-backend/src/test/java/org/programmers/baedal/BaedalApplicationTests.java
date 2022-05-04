8d8e3380616a4363e9597845a739d7571e76e2c3
NownS <sksmsx147@gmail.com>
2022-05-05 02:10:10 +0900

    init Repo

---
+++ baedal-backend/src/test/java/org/programmers/baedal/BaedalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.programmers.baedal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaedalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
